9ade38ecf14306e465b9655f1b02ad700c589bf1
unknown <tayyabjamil777@gmail.com>
2020-11-05 21:55:53 +0500

    first page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HomeComponent } from './home/home.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { CommonModule } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCarouselModule } from '@ngmodule/material-carousel';
+import { NgImageSliderModule } from 'ng-image-slider';
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+      HomeComponent
+   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgImageSliderModule,
+    CommonModule,
+    FlexLayoutModule,
+    BrowserAnimationsModule,
+    MatCarouselModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
