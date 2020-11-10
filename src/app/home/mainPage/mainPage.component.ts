15ae2922455926c2ff65c29e16503f058a22bab0
unknown <tayyabjamil777@gmail.com>
2020-11-10 18:18:13 +0500

    ng

--- src/app/home/mainPage/mainPage.component.ts
+++ src/app/home/mainPage/mainPage.component.ts
@@ -20,7 +20,8 @@ products = []
   deviceLg:boolean;
   deviceMd:boolean;
   deviceSm:boolean;
-  imageObject: Array<object> = [{
+  imageObject: Array<object> = [
+    {
 
     thumbImage: 'assets/images/vegPage.jpg',
     alt: 'alt of image',
@@ -56,22 +57,45 @@ thumbImage: 'assets/images/snacks.jpg',
 title: 'Dairy',
 alt: 'Image alt'
 },
+{
+
+  thumbImage: 'assets/images/vegPage.jpg',
+  alt: 'alt of image',
+  title: 'Dairy',
+
+}, {
+
+  thumbImage: 'assets/images/snacks.jpg',
+  title: 'Bakery & Pastry',
+  alt: 'Image alt'
+},{
+thumbImage: 'assets/images/veg1.jpg',
+alt: 'alt of image',
+title: 'Vegetables'
+}, {
+thumbImage: 'assets/images/bakery.jpg',
+title: 'Image title',
+alt: 'Image alt'
+},{
+thumbImage: 'assets/images/veg1.jpg',
+alt: 'alt of image',
+title: 'Fish & Meat'
+}, {
+thumbImage: 'assets/images/snacks.jpg',
+title: 'Organic',
+alt: 'Image alt'
+},{
+thumbImage: 'assets/images/veg1.jpg',
+alt: 'alt of image',
+title: 'Snakes & Beverages'
+}, {
+thumbImage: 'assets/images/snacks.jpg',
+title: 'Dairy',
+alt: 'Image alt'
+},
 
 ];
-imageObject2: Array<object> = [{
 
-  thumbImage: 'assets/images/slide1.png',
-  title:'NE iamge'
-
-},{
-thumbImage: 'assets/images/slide3.png',
-},{
-
-thumbImage: 'assets/images/slide1.png',
-},{
-thumbImage: 'assets/images/slide3.png',
-}
-];
 
   ngOnInit() {
     this.mediaSub = this.mediaObserver.media$.subscribe((result:MediaChange)=>{
