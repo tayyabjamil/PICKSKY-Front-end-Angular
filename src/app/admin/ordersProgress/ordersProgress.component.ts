106c5501d6b946973a575f8aa449fda1fa88c732
Muhammad Tahir <itstahirjamil@gmail.com>
2021-01-19 22:34:28 +0500

    checkout

--- src/app/admin/ordersProgress/ordersProgress.component.ts
+++ src/app/admin/ordersProgress/ordersProgress.component.ts
@@ -1,6 +1,7 @@
 import { AdminService } from './../admin.service';
 import { Component, OnInit } from '@angular/core';
-import { ProductService } from 'src/app/home/product.service';
+import * as jsPDF from 'jspdf';
+import autoTable from 'jspdf-autotable'
 
 @Component({
   selector: 'app-ordersProgress',
@@ -8,32 +9,125 @@ import { ProductService } from 'src/app/home/product.service';
   styleUrls: ['./ordersProgress.component.scss']
 })
 export class OrdersProgressComponent implements OnInit {
-phase ="delievery";
-  constructor( public adminService:AdminService) { }
-allOrders = []
-ownerEmail;
+  phase = "delievery";
+  constructor(public adminService: AdminService) { }
+  allOrders = []
+  ownerEmail;
+  cartData;
+
   ngOnInit() {
     this.orders()
   }
-  orders(){
 
-   this.adminService.getAllOrders().subscribe((data: any) => {
-    this.allOrders = data.product
+  orders() {
+    this.adminService.getAllOrders().subscribe((data: any) => {
+      this.allOrders = data.product
+    })
+  }
 
-  })
-}
-nextPhase(item){
+  get cartDataValue() {
+    return this.cartData;
+  }
 
-   const orderShipped = {
-     phase : this.phase = "shipping" ,
-     ownerEmail : item.ownerEmail,
-     orderId:item._id
+  nextPhase(item) {
+    this.cartData = item.cartData;
+    setTimeout(() => {
+      this.printOrder(item);
+    }, 1000);
+    const orderShipped = {
+      phase: this.phase = "shipping",
+      ownerEmail: item.ownerEmail,
+      orderId: item._id
+    }
 
-   }
-  this.adminService.shippingPhase(orderShipped).subscribe((data: any) => {
-    alert("Shipping")
+    this.adminService.shippingPhase(orderShipped).subscribe((data: any) => {
+      alert("Shipping")
 
-  })
+    })
 
-}
+  }
+
+  printOrder(data) {
+
+    const doc = new jsPDF.jsPDF()
+
+    // let doc = new jsPDF();
+    autoTable(doc, {
+      theme: 'plain',
+      html: '#tableid',
+      useCss: true,
+      // showHead: 'everyPage',
+      startY: 80,
+    })
+
+    doc.setPage(1)
+    doc.setFont("helvetica");
+    doc.setTextColor("black");
+    doc.setFontSize(17);
+    doc.text('Shirivas Food', 15, 15);
+
+    doc.setPage(1)
+    doc.setTextColor("black");
+    doc.setFontSize(10);
+    doc.text('Name', 15, 35);
+
+    doc.setPage(1)
+    doc.setTextColor("black");
+    doc.setFontSize(10);
+    doc.text((data.firstName.toString() + ' ' + data.lastName.toString()), 15, 40);
+
+    doc.setPage(1)
+    doc.setTextColor("black");
+    doc.setFontSize(10);
+    doc.text('country', 15, 50);
+
+    doc.setPage(1)
+    doc.setTextColor("black");
+    doc.setFontSize(10);
+    doc.text(data.contry.toString(), 15, 55);
+
+    doc.setPage(1)
+    doc.setTextColor("black");
+    doc.setFontSize(10);
+    doc.text('Address', 15, 65);
+
+    doc.setPage(1)
+    doc.setTextColor("black");
+    doc.setFontSize(10);
+    doc.text(data.adress.toString(), 15, 70);
+
+
+    // doc.setPage(1)
+    // doc.setTextColor("black");
+    // doc.setFontSize(10);
+    // doc.text("Order No", 160, 35);
+
+    // doc.setPage(1)
+    // doc.setTextColor("black");
+    // doc.setFontSize(10);
+    // doc.text('122322', 160, 40);
+
+    doc.setPage(1)
+    doc.setTextColor("black");
+    doc.setFontSize(10);
+    doc.text('Status', 160, 35);
+
+    doc.setPage(1)
+    doc.setTextColor("black");
+    doc.setFontSize(10);
+    doc.text(data.phase.toString(), 160, 40);
+
+    doc.setPage(1)
+    doc.setTextColor("black");
+    doc.setFontSize(10);
+    doc.text('Total', 160, 50);
+
+    doc.setPage(1)
+    doc.setTextColor("black");
+    doc.setFontSize(10);
+    doc.text(data.total.toString(), 160, 55);
+
+    // Save the PDF
+    doc.save('Test.pdf');
+  }
 }
--- src/app/checkOut/checkOutPage/checkOutPage.component.ts
+++ src/app/checkOut/checkOutPage/checkOutPage.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit ,ViewChild} from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { AuthService } from 'src/app/auth.service';
 import { CartService } from '../../home/cart.service';
 import { ProductService } from '../../home/product.service';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { IPayPalConfig, ICreateOrderRequest } from 'ngx-paypal';
 import { Router } from '@angular/router';
 import { MatStepper } from '@angular/material/stepper/stepper';
@@ -16,41 +16,47 @@ export class CheckOutPageComponent implements OnInit {
 
 
   constructor(private _formBuilder: FormBuilder,
-    public cartService:CartService,
-     public productService:ProductService,
-     public authService:AuthService,    private router: Router,) { }
+    public cartService: CartService,
+    public productService: ProductService,
+    public authService: AuthService, private router: Router, ) { }
 
 
-     cartItems
-total;
-title = 'newMat';
-showSuccess :any;
-isLinear = true;
-dataOrder;
-firstFormGroup: FormGroup;
-secondFormGroup: FormGroup;
-thirdFormGroup: FormGroup;
-public payPalConfig?: IPayPalConfig;
-refrence:string;
-previousData;
-// @ViewChild('stepper') private stepper: MatStepper;
-@ViewChild('stepper') public stepper: MatStepper;
-ngOnInit() {
+  cartItems;
+
+  printCol = [
+    { name: 'Name' },
+    { name: 'Total' },
+  ];
+
+  total;
+  title = 'newMat';
+  showSuccess: any;
+  isLinear = true;
+  dataOrder;
+  firstFormGroup: FormGroup;
+  secondFormGroup: FormGroup;
+  thirdFormGroup: FormGroup;
+  public payPalConfig?: IPayPalConfig;
+  refrence: string;
+  previousData;
+  // @ViewChild('stepper') private stepper: MatStepper;
+  @ViewChild('stepper') public stepper: MatStepper;
+  ngOnInit() {
     this.initConfig();
     this.getCartItems()
 
     this.firstFormGroup = this._formBuilder.group({
-      email: ['', ],
-      fname: ['', ],
-      lname: ['', ],
-      city: ['', ],
-      adress: ['', ],
-      contry:['',],
-      code:['']
+      email: ['',],
+      fname: ['',],
+      lname: ['',],
+      city: ['',],
+      adress: ['',],
+      contry: ['',],
+      code: ['']
       ,
     });
     this.secondFormGroup = this._formBuilder.group({
-      method: ['', ]
+      method: ['',]
     });
     this.thirdFormGroup = this._formBuilder.group({
       // contact: ['', Validators.required],
@@ -58,115 +64,119 @@ ngOnInit() {
     });
     this.getAllOrders()
   }
-  getCartItems(){
+  getCartItems() {
     this.cartItems = this.cartService.getProducts()
     console.log(this.cartItems)
-   }
-   getImage(imageId) {
+  }
+  getImage(imageId) {
     if (!imageId) return '';
     return this.productService.productImageUrl(imageId);
   }
-   get getTotal() {
-     this.total =  this.cartService.getTotalPrice();
+  get getTotal() {
+    this.total = this.cartService.getTotalPrice();
     return this.total
-   }
-   order(){
+  }
+  order() {
     this.authService.getemail()
     const orderData = {
-      cartItems : this.cartItems,
-      total : this.total,
-      firstName:this.firstFormGroup.value.fname,
-      lastName:this.firstFormGroup.value.lname,
-      city:this.firstFormGroup.value.city,
-      adress:this.firstFormGroup.value.adress,
-      code:this.firstFormGroup.value.code,
-      contry:this.firstFormGroup.value.contry,
-      method:this.secondFormGroup.value.method,
-
+      cartItems: this.cartItems,
+      total: this.total,
+      firstName: this.firstFormGroup.value.fname,
+      lastName: this.firstFormGroup.value.lname,
+      city: this.firstFormGroup.value.city,
+      adress: this.firstFormGroup.value.adress,
+      code: this.firstFormGroup.value.code,
+      contry: this.firstFormGroup.value.contry,
+      method: this.secondFormGroup.value.method,
       refrence: this.refrence,
-      phase:"delievry phase",
+      phase: "delievry phase",
       ownerEmail: this.authService.getemail()
-     }
+    }
+    this.printOrder(orderData);
     this.cartService.order(orderData).subscribe((data: any) => {
-      this.router.navigate(['/'])
-   })
-   this.cartService.emptyProduct()
+      // this.router.navigate(['/'])
+    })
+    // this.cartService.emptyProduct()
   }
-move(index: number) {
-  this.stepper.selectedIndex = index;
-}
-getAllOrders(){
- let id= this.authService.getID()
- this.productService.getOrders().subscribe((data: any) => {
-   this.dataOrder = data.orders
-})
-}
-get useAdress(){
-  if (this.dataOrder) {
-    this.previousData = this.dataOrder[0].adress;
-    return this.previousData;
+  move(index: number) {
+    this.stepper.selectedIndex = index;
   }
-}
 
-private initConfig(): void {
-  this.payPalConfig = {
-  currency: 'EUR',
-  clientId: 'sb',
-  // tslint:disable-next-line: no-angle-bracket-type-assertion
-  createOrderOnClient: (data) => <ICreateOrderRequest>{
-    intent: 'CAPTURE',
-    purchase_units: [
-      {
-        amount: {
-          currency_code: 'EUR',
-          value: '9.99',
-          breakdown: {
-            item_total: {
-              currency_code: 'EUR',
-              value: '9.99'
-            }
-          }
-        },
-        items: [
+  getAllOrders() {
+    let id = this.authService.getID()
+    this.productService.getOrders().subscribe((data: any) => {
+      this.dataOrder = data.orders.toString()
+    })
+  }
+
+  get useAdress() {
+    if (this.dataOrder) {
+      this.previousData = this.dataOrder[0].adress;
+      return this.previousData;
+    }
+  }
+
+  private initConfig(): void {
+    this.payPalConfig = {
+      currency: 'EUR',
+      clientId: 'sb',
+      // tslint:disable-next-line: no-angle-bracket-type-assertion
+      createOrderOnClient: (data) => <ICreateOrderRequest>{
+        intent: 'CAPTURE',
+        purchase_units: [
           {
-            name: 'Enterprise Subscription',
-            quantity: '1',
-            category: 'DIGITAL_GOODS',
-            unit_amount: {
+            amount: {
               currency_code: 'EUR',
               value: '9.99',
+              breakdown: {
+                item_total: {
+                  currency_code: 'EUR',
+                  value: '9.99'
+                }
+              }
             },
+            items: [
+              {
+                name: 'Enterprise Subscription',
+                quantity: '1',
+                category: 'DIGITAL_GOODS',
+                unit_amount: {
+                  currency_code: 'EUR',
+                  value: '9.99',
+                },
+              }
+            ]
           }
         ]
-      }
-    ]
-  },
-  advanced: {
-    commit: 'true'
-  },
-  style: {
-    label: 'paypal',
-    layout: 'vertical'
-  },
-  onApprove: (data, actions) => {
-    console.log('onApprove - transaction was approved, but not authorized', data, actions);
-    actions.order.get().then(details => {
-      console.log('onApprove - you can get full order details inside onApprove: ', details);
-    });
-  },
-  onClientAuthorization: (data) => {
-    console.log('onClientAuthorization - you should probably inform your server about completed transaction at this point', data);
-    this.showSuccess = true;
-  },
-  onCancel: (data, actions) => {
-    console.log('OnCancel', data, actions);
-  },
-  onError: err => {
-    console.log('OnError', err);
-  },
-  onClick: (data, actions) => {
-    console.log('onClick', data, actions);
-  },
-};
-}
+      },
+      advanced: {
+        commit: 'true'
+      },
+      style: {
+        label: 'paypal',
+        layout: 'vertical'
+      },
+      onApprove: (data, actions) => {
+        console.log('onApprove - transaction was approved, but not authorized', data, actions);
+        actions.order.get().then(details => {
+          console.log('onApprove - you can get full order details inside onApprove: ', details);
+        });
+      },
+      onClientAuthorization: (data) => {
+        console.log('onClientAuthorization - you should probably inform your server about completed transaction at this point', data);
+        this.showSuccess = true;
+      },
+      onCancel: (data, actions) => {
+        console.log('OnCancel', data, actions);
+      },
+      onError: err => {
+        console.log('OnError', err);
+      },
+      onClick: (data, actions) => {
+        console.log('onClick', data, actions);
+      },
+    };
+  }
+
+  
 }
